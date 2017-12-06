899d64acc6c1a3fc1c82cb787761ef5dc6de174c
Salim Alam <salam@chef.io>
2017-12-05 19:12:44 -0800

    Test 2

--- app.js
+++ app.js
@@ -7,4 +7,4 @@ if (process.argv.length <= 2) {
 
 var msg = process.argv.slice(2).join(' ');
 console.log(colors.rainbow("Habitat says: " + msg));
-console.log("-- Test 1");
+console.log("-- Test 2");
